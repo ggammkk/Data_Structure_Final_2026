429575d67e1d30fffa766bbe540cfbdc8ca6c6e9
alinanekamanga-lab <alinanekamanga@gmail.com>
2026-05-11 22:18:43 +0800

    u[pdated graph.cpp

--- backend/graph.cpp
+++ backend/graph.cpp
@@ -1,33 +1,53 @@
-#include<iostream>
+#include <iostream>
 #include "graph.h"
 
 using namespace std;
 
-void Graph:: addNode(Node node){
+void Graph::addNode(Node node)
+{
     nodes[node.name] = node;
 }
 
-void Graph:: addEdge(string source, string target, string type){
-    adjacencyList[source].push_back({target,type});
+void Graph::addEdge(string source, string target, string type)
+{
+    adjacencyList[source].push_back({target, type});
 
-    if(type == "parent_of")
-    {adjacencyList[target].push_back({source,"child_of"});}
+    if (type == "parent_of")
+    {
+        adjacencyList[target].push_back({source, "child_of"});
+    }
 
-    else if(type == "uses")
-    {adjacencyList[target].push_back({source,"used_in"});}
+    else if (type == "uses")
+    {
+        adjacencyList[target].push_back({source, "used_in"});
+    }
 
-    else if(type == "is_a")
-    {adjacencyList[target].push_back({source,"child_of"});}
-
+    else if (type == "is_a")
+    {
+        adjacencyList[target].push_back({source, "child_of"});
+    }
 }
 
-void Graph::printGraph(){
-    for(auto& node : adjacencyList){
+void Graph::printGraph()
+{
+    for (auto &node : adjacencyList)
+    {
         cout << node.first << ":\n";
 
-        for(auto& edge : node.second){
+        for (auto &edge : node.second)
+        {
             cout << "  ->" << edge.first << " (" << edge.second << ")\n";
         }
         cout << endl;
     }
+}
+
+unordered_map<string, Node> &Graph::getNodes()
+{
+    return nodes;
+}
+
+unordered_map<string, vector<pair<string, string>>> &Graph::getAdjacencyList()
+{
+    return adjacencyList;
 }
