967fe59d41db96c436be01795c189c84859c06a0
alinanekamanga-lab <alinanekamanga@gmail.com>
2026-05-11 22:46:27 +0800

    started search algorithm

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 
 #include "json.hpp"
 #include "graph.h"
@@ -7,11 +7,13 @@
 using json = nlohmann::json;
 using namespace std;
 
-int main(){
-    
+int main()
+{
+
     ifstream file("../data/dsa_nodes.json");
 
-    if(!file){
+    if (!file)
+    {
         cout << "Failed to open JSON file.\n";
         return 1;
     }
@@ -21,53 +23,61 @@ int main(){
 
     Graph graph;
 
-    for(auto& item : data){
-        
+    for (auto &item : data)
+    {
+
         Node node;
 
-        //basic info
+        // basic info
         node.name = item["name"];
         node.definition = item["definition"];
         node.category = item["category"];
 
-        //operation
-        for(auto& op : item["operations"]){
+        // operation
+        for (auto &op : item["operations"])
+        {
             node.operations.push_back(op);
         }
 
-        //relationship
-        for(auto& rel : item["relationships"]){
+        // relationship
+        for (auto &rel : item["relationships"])
+        {
             Relationship relationship;
             relationship.target = rel["target"];
             relationship.type = rel["type"];
             node.relationships.push_back(relationship);
         }
 
-        //code example
-        for(auto& code : item["code_example"]){
+        // code example
+        for (auto &code : item["code_examples"])
+        {
             node.code_examples.push_back(code);
         }
 
-        //real life example
-        for(auto& example : item["real_life_examples"]){
+        // real life example
+        for (auto &example : item["real_life_examples"])
+        {
             node.real_life_examples.push_back(example);
         }
 
-        //math relations
-        for(auto& math : item["math_relations"]){
+        // math relations
+        for (auto &math : item["math_relations"])
+        {
             node.math_relations.push_back(math);
         }
 
-        //timecomplexity
-        for(auto& tc : item["time_complexity"].items()){
+        // timecomplexity
+        for (auto &tc : item["time_complexity"].items())
+        {
             node.time_complexity[tc.key()] = tc.value();
         }
 
-        //add node
+        // add node
         graph.addNode(node);
 
-        //add relationships
-        for(auto& rel : node.relationships){
+        // add relationships
+        for (auto &rel : node.relationships)
+        {
             graph.addEdge(node.name, rel.target, rel.type);
         }
     }
--- backend/search.cpp
+++ backend/search.cpp
@@ -1,3 +1,84 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include "search.h"
+
+string toLowerCases(string text)
+{
+    transform(text.begin(), text.end(), text.begin(), ::tolower);
+    return text;
+}
+
+vector<Node> Search::searchByName(string name)
+{
+    vector<Node> results;
+    name = toLowerCases(name);
+
+    for (auto &pair : graph->getNodes())
+    {
+        Node node = pair.second;
+
+        if (toLowerCases(node.name).find(name) != string::npos)
+        {
+            results.push_back(node);
+        }
+    }
+    return results;
+}
+
+vector<Node> Search::searchByCategory(string category)
+{
+    vector<Node> results;
+    category = toLowerCases(category);
+
+    for (auto &pair : graph->getNodes())
+    {
+        Node node = pair.second;
+
+        if (toLowerCases(node.category).find(category) != string::npos)
+        {
+            results.push_back(node);
+        }
+    }
+    return results;
+}
+
+vector<Node> Search::searchByOperation(string operation)
+{
+    vector<Node> results;
+    operation = toLowerCases(operation);
+
+    for (auto &pair : graph->getNodes())
+    {
+        Node node = pair.second;
+
+        for (string op : node.operations)
+        {
+            if (toLowerCases(op).find(operation) != string::npos)
+            {
+                results.push_back(node);
+                break;
+            }
+        }
+    }
+    return results;
+}
+
+vector<Node> Search::searchByRelationship(string relationshipType)
+{
+    vector<Node> results;
+    relationshipType = toLowerCases(relationshipType);
+
+    for (auto &pair : graph->getNodes())
+    {
+        Node node = pair.second;
+
+        for (Relationship rel : node.relationships)
+        {
+            if (toLowerCases(rel.type).find(relationshipType) != string::npos)
+            {
+                results.push_back(node);
+                break;
+            }
+        }
+    }
+    return results;
+}
